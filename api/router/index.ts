ea1dcae54fbf69bd4f59adb8e4fb19361ef9738a
Kevin <kevindana99@gmail.com>
2026-02-10 03:47:36 -0300

    fix rootRoute

--- api/router/index.ts
+++ api/router/index.ts
@@ -1,6 +1,8 @@
 import { Express } from "express";
 import v1 from "./v1";
+import rootRoute from "./v1/routes/root";
 const router = (app: Express) => {
+  app.use("/", rootRoute);
   app.use("/api/v1", v1);
 };
 
