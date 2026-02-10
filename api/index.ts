495e345ea5293a38c55a952d3cbac4d98c0b9ce1
Kevin <kevindana99@gmail.com>
2026-02-10 02:18:14 -0300

    config env and env vars and config file

--- api/index.ts
+++ api/index.ts
@@ -4,7 +4,7 @@ import "dotenv/config";
 import express from "express";
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 
 app.get("/", (req, res) => {
   try {
--- config/index.ts
+++ config/index.ts
@@ -1,5 +1,13 @@
-export default {
+type ConfigEnvoirmentType = "production" | "development";
+const PORT = process.env.PORT || 3000;
+const ENVIRONMENT = process.env.NODE_ENV;
+const PRODUCTION = ENVIRONMENT === "production" ? true : false;
+const CONFIG = {
+  PORT,
   ACCESS_TOKEN_TELEGRAM_BOT: process.env.ACCESS_TOKEN_TELEGRAM_BOT,
-  API_HOST: process.env.API_HOST,
-  PORT: process.env.PORT || 3000,
+  API_HOST: PRODUCTION
+    ? `https://${process.env.FLY_APP_NAME}.fly.dev`
+    : `http://localhost:${PORT}`,
 };
+
+export default CONFIG;
