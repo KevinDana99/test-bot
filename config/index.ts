7e5d3dad663ab3b3e97b44b507d8d06ec01cd453
Kevin <kevindana99@gmail.com>
2026-02-10 02:27:47 -0300

    fix config file

--- config/index.ts
+++ config/index.ts
@@ -1,13 +1,16 @@
 type ConfigEnvoirmentType = "production" | "development";
 const PORT = process.env.PORT || 3000;
 const ENVIRONMENT = process.env.NODE_ENV;
-const PRODUCTION = ENVIRONMENT === "production" ? true : false;
+const PRODUCTION =
+  (ENVIRONMENT as ConfigEnvoirmentType) === "production" ? true : false;
 const CONFIG = {
   PORT,
   ACCESS_TOKEN_TELEGRAM_BOT: process.env.ACCESS_TOKEN_TELEGRAM_BOT,
   API_HOST: PRODUCTION
     ? `https://${process.env.FLY_APP_NAME}.fly.dev`
     : `http://localhost:${PORT}`,
+  RAPID_API_HOST: process.env.RAPID_API_HOST,
+  RAPID_API_SECRET_KEY: process.env.RAPID_API_SECRET_KEY,
 };
 
 export default CONFIG;
